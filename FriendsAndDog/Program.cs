75cd397e66b1e65d6d2eb72869eefd0df2a3c39f
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-13 16:23:11 +0300

    fixed

--- FriendsAndDog/Program.cs
+++ FriendsAndDog/Program.cs
@@ -4,26 +4,25 @@ int first_friend_speed = 1;
 int second_friend_speed = 2;
 int dog_speed = 5;
 int friend = 2;
+int time = 0;
 
 while(distance > 10)
 {
     if(friend == 1)
     {
-        int time = distance/(first_friend_speed + dog_speed);
+        time = distance / (first_friend_speed + dog_speed);
         friend = 2;
-        distance = distance - (first_friend_speed + second_friend_speed)*time;
-        count = count + 1;
     }
     
-    if(friend == 2)
+    else
     {
-        int time = distance/(second_friend_speed + dog_speed);
-        friend = 1;
-        distance = distance - (first_friend_speed + second_friend_speed)*time;
-        count = count + 1;
+        time = distance / (second_friend_speed + dog_speed);
+        friend = 1; 
     }
+
+    distance = distance - (first_friend_speed + second_friend_speed) * time;
+    count = count + 1;
+
 }
 
-    Console.Write("Собака пробежит ");
-    Console.Write(count);
-    Console.WriteLine(" раз");
+    Console.WriteLine("Собака пробежит " + count + " раз");
