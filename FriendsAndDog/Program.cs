884cdc1f244668690d126b346a0aa771eebc60f9
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-13 06:52:33 +0300

    add another solution

--- FriendsAndDog/Program.cs
+++ FriendsAndDog/Program.cs
@@ -3,21 +3,21 @@ int distance = 10000;
 int first_friend_speed = 1;
 int second_friend_speed = 2;
 int dog_speed = 5;
-bool friend = true;
+int friend = 2;
 
 while(distance > 10)
 {
-    if(friend = false)
+    if(friend == 1)
     {
         int time = distance/(first_friend_speed + dog_speed);
-        friend = true;
+        friend = 2;
         distance = distance - (first_friend_speed + second_friend_speed)*time;
         count = count + 1;
     }
-    if(friend = true)
+    if(friend == 2)
     {
         int time = distance/(second_friend_speed + dog_speed);
-        friend = false;
+        friend = 1;
         distance = distance - (first_friend_speed + second_friend_speed)*time;
         count = count + 1;
     }
